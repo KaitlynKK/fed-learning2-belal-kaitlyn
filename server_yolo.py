ece603db9c0e6003b09a275d42c380c8510fd4d8
Trick3y <belalnur01@gmail.com>
2025-05-14 21:02:50 +1000

    changes
    changes

--- server_yolo.py
+++ server_yolo.py
@@ -1,10 +1,6 @@
-# server_yolo.py
 import flwr as fl
 
 def aggregate_fit(server_round, results, failures):
-    detections = []
-    for _, _, metrics in results:
-        detections.extend(metrics.get("detections", []))
     return [], {}
 
 strategy = fl.server.strategy.FedAvg(
@@ -12,9 +8,10 @@ strategy = fl.server.strategy.FedAvg(
     fraction_evaluate=0.0,
     min_fit_clients=1,
     min_available_clients=1,
-    on_fit_config_fn=lambda r: {},
-    fit_metrics_aggregation_fn=aggregate_fit,
 )
 
 def main():
     fl.server.start_server(config=fl.server.ServerConfig(num_rounds=1), strategy=strategy)
+
+if __name__ == "__main__":
+    main()
